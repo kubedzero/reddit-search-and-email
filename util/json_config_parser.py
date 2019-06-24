db37b2ca6dcea486183d6dac7307b255b8ee63d8
KZ <kubedzero@gmail.com>
2019-06-24 09:34:00 -0700

    Shorten list flatting logic, handle nested consecutive lists in Json

--- util/json_config_parser.py
+++ util/json_config_parser.py
@@ -7,35 +7,50 @@ from util.log_setup import get_logger_with_name
 # https://www.dummies.com/programming/python/how-to-create-a-constructor-in-python/
 class JsonConfig:
 
-    # Function takes in a search string like "level1.level_2.L3" + a dict and gets the value of the deepest key L3.
-    def __dict_search(self, search_string, remaining_dict):
-        # https://stackoverflow.com/questions/6903557/splitting-on-first-occurrence
-        split_search_string = search_string.split(".", 1)
-        # base case of there being no further nested levels in the search string/dictionary
-        if len(split_search_string) == 1:
-            # dict returns None if the value isn't found
-            return remaining_dict.get(search_string)
+    # Takes a list of nested lists (of nested lists) like [1, [3, [[], 6] ], 7] and flattens
+    # Functionally equivalent to:
+    # def flattenlist(L):
+    #   return [L] if not isinstance(L, list) else [x for X in L for x in flattenlist(X)]
+    def __flatten_list(self, input_object):
+        if isinstance(input_object, list):
+            # declare an empty list
+            flat_list = []
+            # iterate through what we know to be a list
+            for item in input_object:
+                # recursively call each item in the list until the base case. Then add to the flat list and move upward
+                for sub_item in self.__flatten_list(item):
+                    flat_list.append(sub_item)
+            return flat_list
         else:
-            # Recursive case where a dot exists, indicating remaining levels of the search string
-            current_search_string = split_search_string[0]
-            remaining_search_string = split_search_string[1]
-            if current_search_string in remaining_dict:
-                new_value = remaining_dict.get(current_search_string)
-                # get the value and check if it's a list or a dict. if a list, spawn separate recursive and combine
-                # TODO Refactor to support consecutive lists in lists
-                if isinstance(new_value, list):
-                    response_list = [self.__dict_search(remaining_search_string, inner_dict)
-                                     if inner_dict else None for inner_dict in new_value]
-                    if len(response_list) == 1:
-                        return response_list[0]
-                else:
-                    return self.__dict_search(remaining_search_string, new_value)
+            # Base case: wrap the object in a list so it can be iterated through and appended
+            return [input_object]
+
+    # Function takes in a search string like "level1.level_2.L3" + a dict and gets the value of the deepest key L3.
+    def __parsed_json_search(self, search_string, remaining_obj):
+        if isinstance(remaining_obj, list):
+            # Handle the case where we get a list of objects by making a list of their inner contents
+            return [self.__parsed_json_search(search_string, inner_obj)
+                    if inner_obj else None for inner_obj in remaining_obj]
+        else:
+            # https://stackoverflow.com/questions/6903557/splitting-on-first-occurrence
+            split_search_string = search_string.split(".", 1)
+            # base case of there being no further nested levels in the search string/dictionary
+            if len(split_search_string) == 1:
+                # dict returns None if the value isn't found
+                return [remaining_obj.get(search_string)]
             else:
-                # Mirror the behavior of dict.get() when no value can be found
-                return None
+                # Recursive case where a dot exists, indicating remaining levels of the search string
+                current_search_string = split_search_string[0]
+                remaining_search_string = split_search_string[1]
+                # make the recursive call to search the remaining structure with the remaining search string
+                if current_search_string in remaining_obj:
+                    return self.__parsed_json_search(remaining_search_string, remaining_obj.get(current_search_string))
+                else:
+                    # Mirror the behavior of our base case by returning a list
+                    return [None]
 
     # Given a key of a value to look for, find it in the object and return it
-    def get_config_value(self, key):
+    def get_config_value(self, key, singleton_list_convert=True):
         if key == "":
             # https://realpython.com/python-exceptions/
             raise Exception("Empty key passed in, a value cannot be found")
@@ -43,15 +58,18 @@ class JsonConfig:
         # iterate through the config_values list of tuples. If the value is present, return it.
         # Otherwise continue to the fallback config, until we have no more configs left to check
         for config_tuple in self._config_tuples:
-            # search_result can either be a single value or a list of values
-            search_result = self.__dict_search(key, config_tuple[1])
-            # Make sure the result isn't None or that we're not returning a whole list of None
+            # search_result list can be a nested list, so flatten it
+            search_result = self.__flatten_list(self.__parsed_json_search(key, config_tuple[1]))
+
+            # Make sure our result is not a list of only None
             # https://stackoverflow.com/questions/3844801/check-if-all-elements-in-a-list-are-identical
-            # TODO see if this can be simplified
-            if not (isinstance(search_result, list) and search_result[1:] == search_result[:-1] and any(
-                    element is None for element in search_result)) and not (
-                    not isinstance(search_result, list) and search_result is None):
-                return search_result
+            if not (search_result[1:] == search_result[:-1] and any(
+                    element is None for element in search_result)):
+                # For parsing ease by the caller, allow just a value to be returned if the result was a singleton list
+                if len(search_result) == 1 and singleton_list_convert:
+                    return search_result[0]
+                else:
+                    return search_result
         # If there are no configs left to check, the key isn't defined. Throw an exception
         message = "Value could not be found for key [{}]".format(key)
         self._logger_instance.critical(message)
@@ -73,6 +91,10 @@ class JsonConfig:
         self._logger_instance = get_logger_with_name(self._LOG_NAME, "DEBUG")
         # Store a list of tuples of the format ("file_path",dict) so we can track the source of each config
         self._config_tuples = []
+        if not (isinstance(file_path_list, (list, tuple))):
+            message = "Input [{}] is not a list or tuple!".format(file_path_list)
+            self._logger_instance.critical(message)
+            raise Exception(message)
         # iterate through the file path list
         for filename in file_path_list:
             # https://www.guru99.com/python-check-if-file-exists.html
